067b2d65ce6419673232d20ac5715126e9ac65af
ZagSerSan <yan.steys1997@gmail.com>
2023-09-03 20:50:32 +0200

    fix webp

--- src/components/blocks/intro.jsx
+++ src/components/blocks/intro.jsx
@@ -2,6 +2,16 @@ import IconSVG from "../iconSVG"
 // import my_photo from '../../img/intro/photo.png'
 import my_photo from '../../img/intro/photo.webp'
 
+const social = [
+  {iconId: 'mail', subClass: 'linkedin', href: 'https://mailto:yan.steys1997@gmail.com'},
+  {iconId: 'telegram', subClass: 'telegram', href: 'https://t.me/ZagSerSan'},
+  {iconId: 'viber', subClass: 'viber', href: 'https://viber.click/48577782439'},
+  {iconId: 'facebook', subClass: 'facebook', href: 'https://www.facebook.com/profile.php?id=100036791829059'},
+  {iconId: 'instagram', subClass: 'instagram', href: 'https://www.instagram.com/zagsersan/'},
+  {iconId: 'github', subClass: 'github', href: 'https://github.com/ZagSerSan'},
+  {iconId: 'linkedin', subClass: 'linkedin', href: 'https://www.linkedin.com/in/zagsersan/'},
+]
+
 const Intro = () => {
   return (
     <div className="intro">
@@ -13,55 +23,16 @@ const Intro = () => {
               <h1 className="intro-content__name">Serhii Zahorskyi</h1>
               <p className="intro-content__prof">Frontend developer</p>
               <div className="social">
-                <a
-                  href="https://mailto:yan.steys1997@gmail.com"
-                  target="_blank"
-                  className="social__item linkedin"
-                >
-                  <IconSVG iconName='mail'/>
-                </a>
-                <a
-                  href="https://t.me/ZagSerSan"
-                  target="_blank"
-                  className="social__item telegram"
-                >
-                  <IconSVG iconName='telegram'/>
-                </a>
-                <a
-                  href="https://viber.click/48577782439"
-                  target="_blank"
-                  className="social__item viber"
-                >
-                  <IconSVG iconName='viber'/>
-                </a>
-                <a
-                  href="https://www.facebook.com/profile.php?id=100036791829059"
-                  target="_blank"
-                  className="social__item facebook"
-                >
-                  <IconSVG iconName='facebook'/>
-                </a>
-                <a
-                  href="https://www.instagram.com/zagsersan/"
-                  target="_blank"
-                  className="social__item instagram"
-                >
-                  <IconSVG iconName='instagram'/>
-                </a>
-                <a
-                  href="https://github.com/ZagSerSan"
-                  target="_blank"
-                  className="social__item github"
-                >
-                  <IconSVG iconName='github'/>
-                </a>
-                <a
-                  href="https://www.linkedin.com/in/zagsersan/"
-                  target="_blank"
-                  className="social__item linkedin"
-                >
-                  <IconSVG iconName='linkedin'/>
-                </a>
+                {social && social.map((item, index) => (
+                  <a
+                    key={`${index}_${item.iconId}`}
+                    href={item.href}
+                    target="_blank"
+                    className={`social__item ${item.subClass}`}
+                  >
+                    <IconSVG iconName={item.iconId}/>
+                  </a>
+                ))}
               </div>
               {/* <button type="button" className="btn-black" id="btn_hireme">HIRE ME</button> */}
               {/* <button type="button" className="btn-black" id="btn_resume">SEE MY RESUME</button> */}
